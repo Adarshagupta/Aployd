cb31af5221f03e4d226bcc8c3da63a0eb07fa1bf
Adarsh Gupta <blueadarsh1@gmail.com>
2025-03-31 04:39:36 +0530

    push

--- scripts/install.sh
+++ scripts/install.sh
@@ -689,8 +689,8 @@ else
 fi
 
 echo -e "5. Download required files from CDN. "
-curl -fsSL $CDN/docker/docker-compose.yml -o /data/coolify/source/docker-compose.yml
-curl -fsSL $CDN/docker/docker-compose.prod.yml -o /data/coolify/source/docker-compose.prod.yml
+curl -fsSL $CDN/docker-compose.yml -o /data/coolify/source/docker-compose.yml
+curl -fsSL $CDN/docker-compose.prod.yml -o /data/coolify/source/docker-compose.prod.yml
 curl -fsSL $CDN/.env.production -o /data/coolify/source/.env.production
 curl -fsSL $CDN/scripts/upgrade.sh -o /data/coolify/source/upgrade.sh
 
